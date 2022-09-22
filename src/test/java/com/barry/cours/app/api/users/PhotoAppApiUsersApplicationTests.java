233e2fdc29b1e473c518aa4c28e2fc4ca72e047b
algassby <algassby@hotmail.com>
2022-09-22 15:16:52 +0200

    project config

---
+++ src/test/java/com/barry/cours/app/api/users/PhotoAppApiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barry.cours.app.api.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
